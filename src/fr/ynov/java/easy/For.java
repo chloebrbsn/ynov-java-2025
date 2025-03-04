fc8b78e6f2ecb0dbf3e28d9cc3e88d2876a92ec6
BARBANSON Chloé <chloe.barbanson@ynov.com>
2025-03-04 16:32:54 +0100

    for loop

---
+++ src/fr/ynov/java/easy/For.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+public class For {
+
+    int i = 0;
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
