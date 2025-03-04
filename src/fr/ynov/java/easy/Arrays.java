b8a66b5c1b4054524aea0a4af1ca47a53d95c6b0
BARBANSON Chloé <chloe.barbanson@ynov.com>
2025-03-04 17:11:34 +0100

    arrays

---
+++ src/fr/ynov/java/easy/Arrays.java
@@ -0,0 +1,14 @@
+package fr.ynov.java.easy;
+
+public class Arrays {
+
+
+    public static void main(String[] args) {
+        int[] newarray = {1, 2, 3, 4, 5};
+
+        for (int array : newarray) {
+            System.out.println(array);
+        }
+
+    }
+}
