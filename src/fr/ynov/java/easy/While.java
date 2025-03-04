e5245e9de755f8833ab204acf073869ea04a94d6
BARBANSON Chloé <chloe.barbanson@ynov.com>
2025-03-04 16:47:39 +0100

    while loop

---
+++ src/fr/ynov/java/easy/While.java
@@ -0,0 +1,15 @@
+package fr.ynov.java.easy;
+
+public class While {
+
+        int i = 0;
+
+        public static void main(String[] args) {
+            int i = 0;
+
+            while(i<=9) {
+                i++;
+                System.out.println(i);
+            }
+        }
+}
